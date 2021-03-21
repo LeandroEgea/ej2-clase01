f3a85fc6bfa13f21d5b11e8c1ce3104be2b1d4f4
LeandroEgea <leandronicolas.egea@mercadolibre.com>
2021-03-21 19:17:40 -0300

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BienvenidoComponent } from './components/bienvenido/bienvenido.component';
+import { ErrorComponent } from './components/error/error.component';
+import { LoginComponent } from './components/login/login.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'bienvenido-component', component: BienvenidoComponent },
+  { path: 'error-component', component: ErrorComponent },
+  { path: 'login-component', component: LoginComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BienvenidoComponent } from './components/bienvenido/bienvenido.component';
-import { LoginComponent } from './components/login/login.component';
-import { ErrorComponent } from './components/error/error.component';
 
 @NgModule({
-  declarations: [AppComponent, BienvenidoComponent, LoginComponent, ErrorComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
