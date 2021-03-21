93c53f2601e5eaa4751c0693ee8a7d1c1ccd10be
LeandroEgea <leandronicolas.egea@mercadolibre.com>
2021-03-21 14:11:54 -0300

    add components: bienvenido, login and error; remove spec.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BienvenidoComponent } from './components/bienvenido/bienvenido.component';
+import { LoginComponent } from './components/login/login.component';
+import { ErrorComponent } from './components/error/error.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, BienvenidoComponent, LoginComponent, ErrorComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
